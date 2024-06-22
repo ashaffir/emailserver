168da39261ccbec95e896e273454419bb06a7336
Alfred <ashaffir@gmail.com>
2024-06-22 12:21:35 +0000

    fix

--- copy-config.sh
+++ copy-config.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Copy the custom Roundcube configuration to the correct location
-cp /tmp/custom_config.inc.php /var/www/html/config/config.inc.php
+echo "COPY CONFIG FILE..."
+
+echo "ALFS >> this is the origin file location:"
+ls -la /tmp/roundcube-config/
+echo "ALFS >> this is the destination:"
+ls -la /var/www/html/
+cp /tmp/roundcube-config/custom_config.inc.php /var/www/html/config/config.inc.php
 
 # Execute the original entrypoint script for Roundcube
 docker-php-entrypoint apache2-foreground
